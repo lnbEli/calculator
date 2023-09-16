63e27e041d6140fac461bbb6ee27d02632a3986d
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-09-16 11:11:45 +0200

    add message if user tries to divide by zero. add function that allows operator to be used a second time only once an operand has been selected. If calculation exceeds a length of 16 it with change to an exponetial value so it fits on screen.

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 const calcScreenTop = document.querySelector(".calc-screen-top");
 const calcScreenBottom = document.querySelector(".calc-screen-bottom");
-// const calcScreen = document.querySelector(".calc-screen");
-
-// const operandLeft = document.querySelector(".operandLeft");
-// const operandRight = document.querySelector(".operandRight");
-// const operator = document.querySelector(".operator");
-
 let operandLeft;
 let operandRight;
 let operator;
 let answer;
-
-// create div that can be multiplied to be used as buttons of calc
-const button = document.createElement("div");
-const arrayOfButtonOperands = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-const arrayOfButtonsOperators = ["x", "/", "-", "+"];
-const arrayOfButtonExtras = [0, "C", "="];
+let operatorInUse = true;
+let button;
 
 function add(num1, num2) {
-  //   console.log(num1 + num2);
   return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  //   console.log(num1 - num2);
   return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  //console.log(num1 * num2);
   return num1 * num2;
 }
 
 function divide(num1, num2) {
-  //   console.log(num1 / num2);
   return num1 / num2;
 }
 
@@ -47,12 +33,11 @@ function operate(operator, operandLeft, operandRight) {
   } else if (operator === "/") {
     return divide(operandLeft, operandRight);
   } else {
-    console.log("error????"); //should maybe be return?
+    return;
   }
 }
 
 // add event listener for all keys.
-
 const numberButtons = document.querySelectorAll(
   ".calc-button, .calc-button-right "
 );
@@ -61,40 +46,71 @@ numberButtons.forEach((el) =>
   el.addEventListener("click", returnClickedNumber)
 );
 
-//
 function returnClickedNumber() {
-  const button = this.textContent;
+  button = this.textContent;
 
   if (button === "C") {
-    calcScreenTop.textContent = "";
-    calcScreenBottom.textContent = "0";
-    operandLeft = 0;
-    operandRight = 0;
-    operator = 0;
+    clearScreen();
+    clearAllCalculatorVariables();
   } else if (
     button === "X" ||
     button === "-" ||
     button === "/" ||
     button === "+"
   ) {
-    // Left operand & operator assigned
-    operandLeft = calcScreenBottom.textContent;
-    operator = button;
-    calcScreenBottom.textContent += ` ${button} `;
+    //Check if operator has alread been assigned and if it has ignores command.
+    if (operator && operatorInUse) {
+      return;
+    } else if (operator) {
+      calculateAndDisplayResults();
+      updateOperatorOperandLeftAndScreen();
+    } else {
+      updateOperatorOperandLeftAndScreen();
+    }
   } else if (button === "=") {
-    //right operand assigned
-    operandRight = calcScreenBottom.textContent.match(/\d*$/)[0];
-    calcScreenBottom.textContent += ` ${button} `;
-    answer = operate(operator, Number(operandLeft), Number(operandRight));
-    calcScreenTop.textContent = calcScreenBottom.textContent;
-    calcScreenBottom.textContent = answer;
-    //////HERE//////
+    if (operandLeft) {
+      calculateAndDisplayResults();
+    } else {
+      return;
+    }
   } else if (calcScreenBottom.textContent === "0") {
     calcScreenBottom.textContent = button;
+  } else if (operator) {
+    calcScreenBottom.textContent += button;
+    operatorInUse = false;
   } else {
     calcScreenBottom.textContent += button;
   }
 }
 
-//could set operands and operater variable before calculating. Would allow for edits beforehand
-function calculate() {}
+function calculateAndDisplayResults() {
+  operandRight = calcScreenBottom.textContent.match(/\d*$/)[0];
+  calcScreenBottom.textContent += ` = `;
+  answer = operate(operator, Number(operandLeft), Number(operandRight));
+  calcScreenTop.textContent = calcScreenBottom.textContent;
+  if (operator === "/" && operandRight === "0") {
+    calcScreenBottom.textContent = "Don't be a dafty ;)";
+  } else if (calcScreenBottom.textContent.length > 16) {
+    calcScreenBottom.textContent = Number.parseFloat(answer).toExponential(2);
+  } else {
+    calcScreenBottom.textContent = answer;
+  }
+  clearAllCalculatorVariables();
+  operatorInUse = true;
+}
+function clearAllCalculatorVariables() {
+  operator = "";
+  operandLeft = 0;
+  operandRight = 0;
+}
+
+function clearScreen() {
+  calcScreenTop.textContent = "";
+  calcScreenBottom.textContent = "0";
+}
+
+function updateOperatorOperandLeftAndScreen() {
+  operator = button;
+  operandLeft = calcScreenBottom.textContent;
+  calcScreenBottom.textContent += ` ${button} `;
+}
